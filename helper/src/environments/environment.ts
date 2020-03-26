62f7a30fda788e5b02ef493bf6d6beb5824209b1
akiakhil <34807780+akiakhil@users.noreply.github.com>
2020-03-26 18:06:12 +0530

    Hosting changes
    Changes made for hosting the application in Firebase

--- helper/src/environments/environment.ts
+++ helper/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'dont commit with API-Key',
+    projectId: 'helper-c83b5',
+  }
 };
 
 /*
