4e62fbb16e3859213442532cf0ccc3c4caa3c3e9
alvinzach <alvinzach@gmail.com>
2020-03-27 13:58:12 +0530

    added admin module

---
+++ helper/src/app/Modules/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './Components/register/register.component';
+
+
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
