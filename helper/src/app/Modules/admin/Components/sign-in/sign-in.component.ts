2bca0746fd99b50a7b8f37e39dc8e5cc3e3c95b4
akiakhil <34807780+akiakhil@users.noreply.github.com>
2020-03-28 20:21:38 +0530

    ui enhancements

--- helper/src/app/Modules/admin/Components/sign-in/sign-in.component.ts
+++ helper/src/app/Modules/admin/Components/sign-in/sign-in.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../Services/auth.service';
 import { Router } from '@angular/router';
 import { DataService } from '../../Services/data.service';
+declare var $: any;
 
 @Component({
   selector: 'app-sign-in',
@@ -13,39 +14,41 @@ export class SignInComponent implements OnInit {
   success = '';
   error = ''
   processing = ''
-  hey="hello"
+  hey = "hello"
   signinForm = new FormGroup(
     {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)])
     })
-  constructor(public authservice: AuthService,public dataservice:DataService,private router: Router) { }
+  constructor(public authservice: AuthService, public dataservice: DataService, private router: Router) { }
   ngOnInit(): void {
   }
   onSubmit() {
     this.error = '',
-    this.success = ''
+      this.success = ''
     if (!this.signinForm.invalid) {
       this.processing = 'Logging you in ..'
       this.authservice.signin(this.signinForm.value.email, this.signinForm.value.password)
         .then(data => {
           this.success = 'Logged in . Redirecting you to homepage '
           this.processing = ''
-          this.dataservice.getUserData(data.user.uid).subscribe(doc=>{
-            localStorage.setItem("District",doc.get("district"))
-            localStorage.setItem("LocalBody",doc.get("name"))
+          this.dataservice.getUserData(data.user.uid).subscribe(doc => {
+            localStorage.setItem("District", doc.get("district"))
+            localStorage.setItem("LocalBody", doc.get("name"))
             this.router.navigate(['admin/dash'])
           })
         })
         .catch(data => {
-          this.processing=""
+          this.processing = ""
           console.log(data)
           this.error = data.message
+          $('#myModal').modal('show');
         })
     }
     else {
       this.error = "All fields are required"
       console.log(this.error)
+      $('#myModal').modal('show');
     }
   }
 
